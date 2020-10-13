d502b04b5c4890b866abde4622403caa5efab3e5
mcereal <mpbcurtis@gmail.com>
2020-10-13 18:28:38 -0400

    ported classes into classes file

--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,5 @@
 export default {
   APIKey: "&appid=9aab1af0ded7b3e0ee317bd7ff5d58f5",
-  APIUrl: "https://api.openweathermap.org/data/2.5/weather?q=",
+  singleDay: "https://api.openweathermap.org/data/2.5/weather?q=",
+  fiveDay: "https://api.openweathermap.org/data/2.5/forecast?q=",
 };
--- scripts/script.js
+++ scripts/script.js
@@ -1,53 +1,32 @@
 import config from "./config.js";
+import CityWeatherInfo from "./classes.js";
+import RenderWeatherInfo from "./classes.js";
 
 $(document).ready(function () {
   const weatherAPI = config;
+
+  //event listener
   $("#searchBtn").on("click", (event) => {
     let userInput = $("#searchBox").val().trim();
     clickHandler(userInput);
   });
 
-  class CityWeatherInfo {
-    constructor(APIUrl, APIKey, userInput) {
-      this.APIUrl = APIUrl;
-      this.APIKey = APIKey;
-      this.userInput = userInput;
-      this.weatherInfoCall = async function ajaxCall() {
-        let result = await $.ajax({
-          url: `${this.APIUrl}${this.userInput}${this.APIKey}`,
-          method: "GET",
-        });
-        return result;
-      };
-    }
-    getWeatherInfo() {
-      return this.weatherInfoCall();
-    }
-  }
-
-  async function renderWeatherInfo(city) {
-    await Promise.resolve(city).then((city) => {
-      $("#city").text(`${city.name}`);
-      $("#date").text(` ${timeStampConverter(city.dt)}`);
-      $("#temperature").text(
-        `${Math.round((city.main.temp - 273) * 1.8 + 32)} \xB0 F`
-      );
-      $("#humidity").text(`${city.main.humidity}`);
-      $("#windSpeed").text(`${city.wind.speed} mph`);
-      $("#uVIndex").text(`${city.main.humidity}`);
-    });
-  }
-
+  //creates new city object and displays it on page
   function clickHandler(userInput) {
-    let newCityInfo = new CityWeatherInfo(
-      weatherAPI.APIUrl,
+    console.log(CityWeatherInfo);
+    let newSingleDayForecast = new CityWeatherInfo.CityWeatherInfo(
+      weatherAPI.singleDay,
       weatherAPI.APIKey,
       userInput
     );
-    renderWeatherInfo(newCityInfo.getWeatherInfo());
-  }
-
-  function timeStampConverter(timestamp) {
-    return moment.unix(timestamp).format("MM/DD/YYYY");
+    let newFiveDayForecast = new CityWeatherInfo.CityWeatherInfo(
+      weatherAPI.fiveDay,
+      weatherAPI.APIKey,
+      userInput
+    );
+    let newRenderForecast = new RenderWeatherInfo.RenderWeatherInfo(
+      newSingleDayForecast.getWeatherInfo()
+    );
+    newRenderForecast.getRenderTemplate();
   }
 });
