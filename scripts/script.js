64273cea90c004c4966284158dbe872534fe75dd
mcereal <mpbcurtis@gmail.com>
2020-10-13 16:35:46 -0400

    All city info renders except uv index

--- scripts/script.js
+++ scripts/script.js
@@ -25,51 +25,29 @@ $(document).ready(function () {
     }
   }
 
+  async function renderWeatherInfo(city) {
+    await Promise.resolve(city).then((city) => {
+      $("#city").text(`${city.name}`);
+      $("#date").text(` ${timeStampConverter(city.dt)}`);
+      $("#temperature").text(
+        `${Math.round((city.main.temp - 273) * 1.8 + 32)} \xB0 F`
+      );
+      $("#humidity").text(`${city.main.humidity}`);
+      $("#windSpeed").text(`${city.wind.speed} mph`);
+      $("#uVIndex").text(`${city.main.humidity}`);
+    });
+  }
+
   function clickHandler(userInput) {
     let newCityInfo = new CityWeatherInfo(
       weatherAPI.APIUrl,
       weatherAPI.APIKey,
       userInput
     );
-
-    async function consoleTest(city) {
-      await Promise.resolve(city).then((city) => {
-        $("#temperature").text(`${city.main.temp}`);
-      });
-    }
-    consoleTest(newCityInfo.getWeatherInfo());
+    renderWeatherInfo(newCityInfo.getWeatherInfo());
   }
 
-  // myCity.getWeatherInfo();
-  // console.log(myCity.getWeatherInfo());
-
-  // $.ajax({
-  //   url: weatherAPI.APIUrl,
-  //   method: "GET",
-  // })
-  //   // We store all of the retrieved data inside of an object called "response"
-  //   .then(function (response) {
-  //     // Log the queryURL
-  //     console.log(weatherAPI);
-
-  //     // Log the resulting object
-  //     console.log(response);
-
-  //     // Transfer content to HTML
-  //     $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-  //     $(".wind").text("Wind Speed: " + response.wind.speed);
-  //     $(".humidity").text("Humidity: " + response.main.humidity);
-
-  //     // Convert the temp to fahrenheit
-  //     var tempF = (response.main.temp - 273.15) * 1.8 + 32;
-
-  //     // add temp content to html
-  //     $(".temp").text("Temperature (K) " + response.main.temp);
-  //     $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
-
-  //     // Log the data in the console as well
-  //     console.log("Wind Speed: " + response.wind.speed);
-  //     console.log("Humidity: " + response.main.humidity);
-  //     console.log("Temperature (F): " + tempF);
-  //   });
+  function timeStampConverter(timestamp) {
+    return moment.unix(timestamp).format("MM/DD/YYYY");
+  }
 });
